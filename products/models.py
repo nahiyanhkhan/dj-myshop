08d202bd5a7c9ff5ec216509631a8afba62c635a
nahiyanhkhan <nahiyanhkhan@gmail.com>
2024-10-01 12:33:57 +0600

    Module 18.2 done

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    is_active = models.BooleanField(default=True)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- products/views.py
+++ products/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.db.models import Count, Avg
+from .models import Category, Product
+
 
 # Create your views here.
+def index(request):
+    # products = Product.objects.all().order_by("price") # Ascending order
+    # products = Product.objects.all().order_by("-price") # Descending order
+
+    # categories = Category.objects.all().annotate(product_count=Count("product"))
+    categories = (
+        Category.objects.all()
+        .annotate(product_count=Count("product"))
+        .prefetch_related("product_set")
+    )
+
+    products = (
+        Product.objects.all().order_by("price", "title").select_related("category")
+    )
+    # products = Product.objects.filter(category__title="Electronics")
+
+    price_avg = products.aggregate(Avg("price"))["price__avg"]
+    # price_avg = Product.objects.raw("SELECT AVG(price) FROM products_product")
+
+    context = {
+        "categories": categories,
+        "products": products,
+        "price_avg": round(price_avg, 2),
+    }
+    return render(request, "products/index.html", context=context)
